93e59cba6331ccdcecd098d5b9b2d1ba28ed0414
wfelipeagudelo <wfelipeagudelo@gmail.com>
2021-06-13 10:49:25 +0200

    Base Données v.0.2.2 2éme table crée mais erreur dans base.html ligne 53 pour la valeur id_telephone

--- APP_FILMS/gestion_tel_pers/gestion_tel_personne_crud.py
+++ APP_FILMS/gestion_tel_pers/gestion_tel_personne_crud.py
@@ -1,5 +1,5 @@
 """
-    Fichier : gestion_ustensile_plat_crud.py
+    Fichier : gestion_tel_personne_crud.py
     Auteur : OM 2021.05.01
     Gestions des "routes" FLASK et des données pour l'association entre les films et les ingredient.
 """
@@ -19,9 +19,8 @@ from APP_FILMS.erreurs.exceptions import *
 from APP_FILMS.erreurs.msg_erreurs import *
 
 """
-    Nom : films_genres_afficher
-    Auteur : OM 2021.05.01
-    Définition d'une "route" /films_genres_afficher
+    Nom : tel_personne_afficher
+    Définition d'une "route" /tel_personne_afficher
     
     But : Afficher les films avec les ingredient associés pour chaque film.
     
@@ -31,8 +30,8 @@ from APP_FILMS.erreurs.msg_erreurs import *
 """
 
 
-@obj_mon_application.route("/mail_personne_afficher/<int:id_mail>", methods=['GET', 'POST'])
-def mail_personne_afficher(id_mail):
+@obj_mon_application.route("/tel_personne_afficher/<int:id_telephone>", methods=['GET', 'POST'])
+def tel_personne_afficher(id_telephone):
     if request.method == "GET":
         try:
             try:
@@ -48,20 +47,20 @@ def mail_personne_afficher(id_mail):
 
             with MaBaseDeDonnee().connexion_bd.cursor() as mc_afficher:
 
-                strsql_ingredient_film_afficher_data = """SELECT id_mail, mail, GROUP_CONCAT(prenom_pers, " ",nom_pers) as NomPers FROM t_pers_mail
-                                                            RIGHT JOIN t_personne ON t_personne.id_personne = t_pers_mail.fk_personne
-                                                            RIGHT JOIN t_mail ON t_mail.id_mail = t_pers_mail.fk_mail
-                                                            GROUP BY id_mail"""
-                if id_mail == 0:
-                    # le paramètre 0 permet d'afficher tous les films
-                    # Sinon le paramètre représente la valeur de l'id du film
+                strsql_ingredient_film_afficher_data = """SELECT id_telephone, num_telephone, GROUP_CONCAT(prenom_pers, " ",nom_pers) as NomPers FROM t_pers_telephone
+                                                            RIGHT JOIN t_personne ON t_personne.id_personne = t_pers_telephone.fk_personne
+                                                            RIGHT JOIN t_telephone ON t_telephone.id_telephone = t_pers_telephone.fk_telephone
+                                                            GROUP BY id_telephone"""
+                if id_telephone == 0:
+                    # le paramètre 0 permet d'afficher tous les telephones
+                    # Sinon le paramètre représente la valeur de l'id du telephone
                     mc_afficher.execute(strsql_ingredient_film_afficher_data)
                 else:
-                    # Constitution d'un dictionnaire pour associer l'id du film sélectionné avec un nom de variable
-                    valeur_id_film_selected_dictionnaire = {"value_id_film_selected": id_mail}
+                    # Constitution d'un dictionnaire pour associer l'id du telephone sélectionné avec un nom de variable
+                    valeur_id_film_selected_dictionnaire = {"value_id_film_selected": id_telephone}
                     # En MySql l'instruction HAVING fonctionne comme un WHERE... mais doit être associée à un GROUP BY
                     # L'opérateur += permet de concaténer une nouvelle valeur à la valeur de gauche préalabﬁement définie.
-                    strsql_ingredient_film_afficher_data += """ HAVING id_mail= %(value_id_film_selected)s"""
+                    strsql_ingredient_film_afficher_data += """ HAVING id_telephone= %(value_id_film_selected)s"""
 
                     mc_afficher.execute(strsql_ingredient_film_afficher_data, valeur_id_film_selected_dictionnaire)
 
@@ -70,34 +69,34 @@ def mail_personne_afficher(id_mail):
                 print("data_genres ", data_genres_films_afficher, " Type : ", type(data_genres_films_afficher))
 
                 # Différencier les messages.
-                if not data_genres_films_afficher and id_mail == 0:
-                    flash("""La table "t_mail" est vide. !""", "warning")
-                elif not data_genres_films_afficher and id_mail > 0:
+                if not data_genres_films_afficher and id_telephone == 0:
+                    flash("""La table "t_telephone" est vide. !""", "warning")
+                elif not data_genres_films_afficher and id_telephone > 0:
                     # Si l'utilisateur change l'id_film dans l'URL et qu'il ne correspond à aucun film
-                    flash(f"Le mail {id_mail} demandé n'existe pas !!", "warning")
+                    flash(f"Le telephone {id_telephone} demandé n'existe pas !!", "warning")
                 else:
-                    flash(f"Données mails et personnes affichés !!", "success")
+                    flash(f"Données téléphone et personnes affichés !!", "success")
 
         except Exception as Exception_films_genres_afficher:
             code, msg = Exception_films_genres_afficher.args
             flash(f"{error_codes.get(code, msg)} ", "danger")
-            flash(f"Exception mails_personnes_afficher : {sys.exc_info()[0]} "
+            flash(f"Exception tel_personnes_afficher : {sys.exc_info()[0]} "
                   f"{Exception_films_genres_afficher.args[0]} , "
                   f"{Exception_films_genres_afficher}", "danger")
 
     # Envoie la page "HTML" au serveur.
-    return render_template("gestion_mail_pers/mail_personne_afficher.html", data=data_genres_films_afficher)
+    return render_template("gestion_tel_pers/tel_personne_afficher.html", data=data_genres_films_afficher)
 
 
 """
-    nom: edit_genre_film_selected
+    nom: edit_personne_tel_selected
     On obtient un objet "objet_dumpbd"
 
     Récupère la liste de tous les ingredient du film sélectionné par le bouton "MODIFIER" de "ustensile_plat_afficher.html"
     
     Dans une liste déroulante particulière (tags-selector-tagselect), on voit :
     1) Tous les ingredient contenus dans la "t_genre".
-    2) Les ingredient attribués au film selectionné.
+    2) Les ingredient attribués au téléphone selectionné.
     3) Les ingredient non-attribués au film sélectionné.
 
     On signale les erreurs importantes
@@ -105,12 +104,12 @@ def mail_personne_afficher(id_mail):
 """
 
 
-@obj_mon_application.route("/edit_mail_pers_selected", methods=['GET', 'POST'])
-def edit_mail_pers_selected():
+@obj_mon_application.route("/edit_tel_pers_selected", methods=['GET', 'POST'])
+def edit_tel_pers_selected():
     if request.method == "GET":
         try:
             with MaBaseDeDonnee().connexion_bd.cursor() as mc_afficher:
-                strsql_genres_afficher = """SELECT * FROM t_mail ORDER BY id_mail ASC"""
+                strsql_genres_afficher = """SELECT * FROM t_telephone ORDER BY id_telephone ASC"""
                 mc_afficher.execute(strsql_genres_afficher)
             data_genres_all = mc_afficher.fetchall()
             print("dans edit_genre_film_selected ---> data_genres_all", data_genres_all)
@@ -119,7 +118,7 @@ def edit_mail_pers_selected():
             # l'utilisateur clique sur le bouton "Modifier" et on récupère la valeur de "id_film"
             # grâce à la variable "id_film_genres_edit_html" dans le fichier "ustensile_plat_afficher.html"
             # href="{{ url_for('edit_genre_film_selected', id_film_genres_edit_html=row.id_film) }}"
-            id_film_genres_edit = request.values['id_mail_edit']
+            id_film_genres_edit = request.values['id_telephone_edit']
 
             # Mémorise l'id du mail dans une variable de session
             # (ici la sécurité de l'application n'est pas engagée)
@@ -127,7 +126,7 @@ def edit_mail_pers_selected():
             session['session_id_film_genres_edit'] = id_film_genres_edit
 
             # Constitution d'un dictionnaire pour associer l'id du mail sélectionné avec un nom de variable
-            valeur_id_film_selected_dictionnaire = {"value_id_mail_selected": id_film_genres_edit}
+            valeur_id_film_selected_dictionnaire = {"value_id_telephone_selected": id_film_genres_edit}
 
             # Récupère les données grâce à 3 requêtes MySql définie dans la fonction ingredient_film_afficher_data
             # 1) Sélection du film choisi
@@ -138,7 +137,7 @@ def edit_mail_pers_selected():
                 ingredient_film_afficher_data(valeur_id_film_selected_dictionnaire)
 
             print(data_genre_film_selected)
-            lst_data_film_selected = [item['id_mail'] for item in data_genre_film_selected]
+            lst_data_film_selected = [item['id_telephone'] for item in data_genre_film_selected]
             print("lst_data_film_selected  ", lst_data_film_selected,
                   type(lst_data_film_selected))
 
@@ -164,18 +163,18 @@ def edit_mail_pers_selected():
 
             # Extrait les valeurs contenues dans la table "t_personnes", colonne "nom_pers"
             # Le composant javascript "tagify" pour afficher les tags n'a pas besoin de l'id_persnne
-            lst_data_genres_films_non_attribues = [item['nom_pers'] for item in data_genres_films_non_attribues]
+            lst_data_genres_films_non_attribues = [item['num_telephone'] for item in data_genres_films_non_attribues]
             print("lst_all_genres gf_edit_genre_film_selected ", lst_data_genres_films_non_attribues,
                   type(lst_data_genres_films_non_attribues))
 
         except Exception as Exception_edit_genre_film_selected:
             code, msg = Exception_edit_genre_film_selected.args
             flash(f"{error_codes.get(code, msg)} ", "danger")
-            flash(f"Exception edit_genre_film_selected : {sys.exc_info()[0]} "
+            flash(f"Exception edit_tel_pers_selected : {sys.exc_info()[0]} "
                   f"{Exception_edit_genre_film_selected.args[0]} , "
                   f"{Exception_edit_genre_film_selected}", "danger")
 
-    return render_template("gestion_mail_pers/mail_personne_modifier_tags_dropbox.html",
+    return render_template("gestion_tel_pers/tel_personne_modifier_tags_dropbox.html",
                            data_genres=data_genres_all,
                            data_film_selected=data_genre_film_selected,
                            data_genres_attribues=data_genres_films_attribues,
@@ -196,8 +195,8 @@ def edit_mail_pers_selected():
 """
 
 
-@obj_mon_application.route("/update_mail_personne_selected", methods=['GET', 'POST'])
-def update_mail_personne_selected():
+@obj_mon_application.route("/update_tel_personne_selected", methods=['GET', 'POST'])
+def update_tel_personne_selected():
     if request.method == "POST":
         try:
             # Récupère l'id du film sélectionné
@@ -228,40 +227,40 @@ def update_mail_personne_selected():
 
             # Pour apprécier la facilité de la vie en Python... "les ensembles en Python"
             # https://fr.wikibooks.org/wiki/Programmation_Python/Ensembles
-            # OM 2021.05.02 Une liste de "id_genre" qui doivent être effacés de la table intermédiaire "t_genre_film".
+            # OM 2021.05.02 Une liste de "id_genre" qui doivent être effacés de la table intermédiaire "t_pers_telephone".
             lst_diff_genres_delete_b = list(
                 set(old_lst_data_genres_films_attribues) - set(new_lst_int_genre_film_old))
             print("lst_diff_genres_delete_b ", lst_diff_genres_delete_b)
 
-            # Une liste de "id_genre" qui doivent être ajoutés à la "t_genre_film"
+            # Une liste de "id_genre" qui doivent être ajoutés à la "t_pers_telephone"
             lst_diff_genres_insert_a = list(
                 set(new_lst_int_genre_film_old) - set(old_lst_data_genres_films_attribues))
             print("lst_diff_genres_insert_a ", lst_diff_genres_insert_a)
 
             # SQL pour insérer une nouvelle association entre
-            # "fk_mail"/"id_mail" et "fk_personne"/"id_personne" dans la "t_pers_mail"
-            strsql_insert_genre_film = """INSERT INTO t_pers_mail (fk_personne, fk_mail) VALUES (%(value_fk_personne)s, %(value_fk_mail)s)"""
+            # "fk_telephone"/"id_telephone" et "fk_personne"/"id_personne" dans la "t_pers_telephone"
+            strsql_insert_genre_film = """INSERT INTO t_pers_telephone (fk_personne, fk_telephone) VALUES (%(value_fk_personne)s, %(value_fk_telephone)s)"""
 
-            # SQL pour effacer des association(s) existantes entre "id_mail" et "id_personne" dans la "t_pers_mail"
-            strsql_delete_genre_film = """DELETE FROM t_pers_mail WHERE fk_personne = %(value_fk_personne)s AND fk_mail = %(value_fk_mail)s"""
+            # SQL pour effacer des association(s) existantes entre "id_telephone" et "id_personne" dans la "t_pers_telephone"
+            strsql_delete_genre_film = """DELETE FROM t_pers_telephone WHERE fk_personne = %(value_fk_personne)s AND fk_telephone = %(value_fk_telephone)s"""
 
             with MaBaseDeDonnee() as mconn_bd:
-                # Pour le mail sélectionné, parcourir la liste des ingredient à INSÉRER dans la "t_pers_mail".
+                # Pour le mail sélectionné, parcourir la liste des ingredient à INSÉRER dans la "t_pers_telephone".
                 # Si la liste est vide, la boucle n'est pas parcourue.
                 for id_genre_ins in lst_diff_genres_insert_a:
-                    # Constitution d'un dictionnaire pour associer l'id du mail sélectionné avec un nom de variable
+                    # Constitution d'un dictionnaire pour associer l'id du telephone sélectionné avec un nom de variable
                     # et "id_personne_ins" (l'id du genre dans la liste) associé à une variable.
-                    valeurs_film_sel_genre_sel_dictionnaire = {"value_fk_mail": id_film_selected,
+                    valeurs_film_sel_genre_sel_dictionnaire = {"value_fk_telephone": id_film_selected,
                                                                "value_fk_personne": id_genre_ins}
 
                     mconn_bd.mabd_execute(strsql_insert_genre_film, valeurs_film_sel_genre_sel_dictionnaire)
 
-                # Pour le film sélectionné, parcourir la liste des ingredient à EFFACER dans la "t_pers_mail".
+                # Pour le telephone sélectionné, parcourir la liste des personnes à EFFACER dans la "t_pers_telephone".
                 # Si la liste est vide, la boucle n'est pas parcourue.
                 for id_genre_del in lst_diff_genres_delete_b:
                     # Constitution d'un dictionnaire pour associer l'id du mail sélectionné avec un nom de variable
                     # et "id_personne_del" (l'id du personne dans la liste) associé à une variable.
-                    valeurs_film_sel_genre_sel_dictionnaire = {"value_fk_mail": id_film_selected,
+                    valeurs_film_sel_genre_sel_dictionnaire = {"value_fk_telephone": id_film_selected,
                                                                "value_fk_personne": id_genre_del}
 
                     # Du fait de l'utilisation des "context managers" on accède au curseur grâce au "with".
@@ -273,19 +272,19 @@ def update_mail_personne_selected():
         except Exception as Exception_update_genre_film_selected:
             code, msg = Exception_update_genre_film_selected.args
             flash(f"{error_codes.get(code, msg)} ", "danger")
-            flash(f"Exception update_personne_mail_selected : {sys.exc_info()[0]} "
+            flash(f"Exception update_personne_tel_selected : {sys.exc_info()[0]} "
                   f"{Exception_update_genre_film_selected.args[0]} , "
                   f"{Exception_update_genre_film_selected}", "danger")
 
     # Après cette mise à jour de la table intermédiaire "t_genre_film",
     # on affiche les films et le(urs) genre(s) associé(s).
-    return redirect(url_for('mail_personne_afficher', id_mail=id_film_selected))
+    return redirect(url_for('tel_personne_afficher', id_telephone=id_film_selected))
 
 
 """
-    nom: personne_mail_afficher_data
+    nom: personne_tel_afficher_data
 
-    Récupère la liste de tous les ingredient du film sélectionné par le bouton "MODIFIER" de "personne_mail_afficher.html"
+    Récupère la liste de tous les tele du film sélectionné par le bouton "MODIFIER" de "personne_tel_afficher.html"
     Nécessaire pour afficher tous les "TAGS" des personnes, ainsi l'utilisateur voit les personnes à disposition
     
     On signale les erreurs importantes
@@ -296,18 +295,18 @@ def ingredient_film_afficher_data(valeur_id_film_selected_dict):
     print("valeur_id_film_selected_dict...", valeur_id_film_selected_dict)
     try:
 
-        strsql_film_selected = """SELECT id_mail, mail FROM t_mail WHERE id_mail = %(value_id_mail_selected)s"""
+        strsql_film_selected = """SELECT id_telephone, num_telephone FROM t_telephone WHERE id_telephone = %(value_id_telephone_selected)s"""
 
         strsql_genres_films_non_attribues = """SELECT id_personne, prenom_pers, nom_pers FROM t_personne 
-                                                    WHERE id_personne not in(SELECT id_personne as idPersonneMail FROM t_pers_mail
-                                                    INNER JOIN t_personne ON t_personne.id_personne = t_pers_mail.fk_personne
-                                                    INNER JOIN t_mail ON t_mail.id_mail = t_pers_mail.fk_mail
-                                                    WHERE id_mail = %(value_id_mail_selected)s)"""
+                                                    WHERE id_personne not in(SELECT id_personne as idPersonneTel FROM t_pers_telephone
+                                                    INNER JOIN t_personne ON t_personne.id_personne = t_pers_telephone.fk_personne
+                                                    INNER JOIN t_telephone ON t_telephone.id_telephone = t_pers_telephone.fk_telephone
+                                                    WHERE id_telephone = %(value_id_telephone_selected)s)"""
 
-        strsql_genres_films_attribues = """SELECT id_personne, nom_pers, prenom_pers FROM t_pers_mail
-                                            INNER JOIN t_personne ON t_personne.id_personne = t_pers_mail.fk_personne
-                                            INNER JOIN t_mail ON t_mail.id_mail = t_pers_mail.fk_mail
-                                            WHERE id_mail = %(value_id_mail_selected)s"""
+        strsql_genres_films_attribues = """SELECT id_personne, nom_pers, prenom_pers FROM t_pers_telephone
+                                            INNER JOIN t_personne ON t_personne.id_personne = t_pers_telephone.fk_personne
+                                            INNER JOIN t_telephone ON t_telephone.id_telephone = t_pers_telephone.fk_telephone
+                                            WHERE id_telephone = %(value_id_telephone_selected)s"""
 
         # Du fait de l'utilisation des "context managers" on accède au curseur grâce au "with".
         with MaBaseDeDonnee().connexion_bd.cursor() as mc_afficher:
